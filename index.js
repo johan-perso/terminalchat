1de8a729b0c99de9a8818a16c9976c2dd50f49bc
Johan <41506568+johan-perso@users.noreply.github.com>
2020-12-29 16:53:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,29 @@ const client = new Discord.Client(); // http://npmjs.com/package/discord.js
 var term = require('terminal-kit').terminal; // https://www.npmjs.com/package/terminal-kit
 var config = require('./config.json') // Fichier local
 
+// Vérification du pseudo + censure si besoin
+if(config.pseudo.includes("INFORMATION") || config.pseudo.includes("Arrivage") || config.pseudo.includes("*")){
+    var pseudo = "##########"
+} else {
+    var pseudo = config.pseudo
+}
+
 // Indication du pseudo
 term("Votre pseudo : ") 
-term.cyan(config.pseudo + "\n")
+term.cyan(pseudo + "\n")
 
 // Connexion au bot Discord
-client.login(config.token).catch(err => {
+var tokenList = config.token // Liste des token
+var token = tokenList[Math.floor(Math.random() * tokenList.length)] // Definition du token utilisé
+
+client.login(token).catch(err => {  
   term.red("Token de bot Discord invalide : Contactez votre fournisseur de terminalchat")
   process.exit()
 });
  
 // Quand le bot est "prêt", Lancer la fonction send (Demande de texte puis envoie d'un message sur le discord)
 client.on('ready', () => {
+    client.channels.cache.get(config.channelId).send("Arrivage " + pseudo + " vient de rejoindre le chat.")
     send()
     function send() {
     term.inputField(function(error, text){
@@ -27,29 +38,45 @@ client.on('ready', () => {
           term.red("Message vide... ")
           return send()
         }
-        client.channels.cache.get(config.channelId).send(config.pseudo + " : " + text)
+        client.channels.cache.get(config.channelId).send(pseudo + " : " + text)
         send()
     });
 }
 });
 
-// Quand on voit un message, L'afficher dans Terminalchat
+// Quand on voit un message, L'afficher dans Terminalchat sauf si le message commence par quelque chose en particulier
 client.on('message', msg => {
     const args = msg.content.slice().trim().split('');
     var message = args.join('')
-    term(message + "\n")
+
+    if(message.startsWith("INFORMATION :")){
+        term.red(message + "\n\n\n")
+    } else {
+    if(message.startsWith("Arrivage")){
+        term.yellow(message.replace("Arrivage ", ""))
+        term.yellow("\n")
+    } else {
+    if(message.startsWith("*shutdown")){
+        term.red("Arrêt de Terminalchat demandé par " + msg.author.username + "\n")
+        client.channels.cache.get(config.channelId).send("Arrivage " + pseudo + " vient de quitter le chat.").then(msg => process.exit())
+    } else {
+        term(message + "\n")
+    }
+    }
+    }
+
 });
 
 // Si on appuie sur CTRL_Z / CTRL_C / CTRL_D, Arrêter le processus
 term.grabInput(true);
 term.on('key', function(name, matches, data){
   if (name === 'CTRL_Z'){
-		process.exit();
+    client.channels.cache.get(config.channelId).send("Arrivage " + pseudo + " vient de quitter le chat.").then(msg => process.exit())
   }
   if (name === 'CTRL_C'){
-    process.exit();
+    client.channels.cache.get(config.channelId).send("Arrivage " + pseudo + " vient de quitter le chat.").then(msg => process.exit())
   }
   if (name === 'CTRL_D'){
-		process.exit();
+	client.channels.cache.get(config.channelId).send("Arrivage " + pseudo + " vient de quitter le chat.").then(msg => process.exit())
 	}
   });
